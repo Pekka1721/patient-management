0b4dc51f3744d84dfc62867c5a992a03f19b5d59
rajender <pekkarange1721@gmail.com>
2025-05-07 19:01:14 +0530

    billing service grpc C_S

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsExecption;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -20,8 +21,11 @@ public class PatientService {
     //dependency injection passing to construction pattern
 
     private PatientRepository patientRepository;
-    public PatientService(PatientRepository patientRepository){
+    private BillingServiceGrpcClient billingServiceGrpcClient;
+
+    public PatientService(PatientRepository patientRepository,BillingServiceGrpcClient billingServiceGrpcClient){
         this.patientRepository=patientRepository;
+        this.billingServiceGrpcClient=billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getPatients(){
@@ -35,6 +39,7 @@ public class PatientService {
             throw new EmailAlreadyExistsExecption("A patient with this email "+"already exists "+ patientRequestDTO.getEmail());
         }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),newPatient.getName(),newPatient.getEmail());
         return PatientMapper.toDTO(newPatient);
     }
 
